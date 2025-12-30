09d6822f8798b09e3c7276eaac5327236f7e12ea
munew <mune3@proton.me>
2025-12-30 23:26:38 +0100

    feat: disable class check hook

--- includes/offsets.h
+++ includes/offsets.h
@@ -7,4 +7,6 @@
 // https://github.com/openjdk/jdk/blob/92c6799b401eb786949e88cd7142002b2a875ce0/src/hotspot/os/windows/os_windows.cpp#L1716
 #define VA_JVM_WINDOWS_OS_DLL_LOAD 0x826C690
 
+#define VA_JVM_CLASS_CHECK 0x813C790
+
 #endif
--- src/hook.c
+++ src/hook.c
@@ -32,6 +32,13 @@ void* hk_windows_os_dll_load(const char *name, char *ebuf, int ebuflen) {
 	return NULL;
 }
 
+typedef bool(*t_class_check)(void* jstr);
+static t_class_check orig_class_check = NULL;
+bool hk_class_check(void* jstr) {
+	(void)jstr;
+	return false;
+}
+
 bool initialize_hooks() {
 	if (MH_Initialize() != MH_OK) {
 		OutputDebugStringA("disabler: Failed to initialize MinHook");
@@ -48,6 +55,7 @@ bool initialize_hooks() {
 	uintptr_t jvm_addr = (uintptr_t) jvm_module;
 	void* jni_get_created_java_vms = (void*)GetProcAddress(jvm_module, "JNI_GetCreatedJavaVMs");
 	void* windows_os_dll_load = (void*) (jvm_addr + VA_JVM_WINDOWS_OS_DLL_LOAD - JVM_BASE_IMAGE);
+	void* class_check = (void*) (jvm_addr + VA_JVM_CLASS_CHECK - JVM_BASE_IMAGE);
 
 	if (MH_CreateHook(jni_get_created_java_vms, (LPVOID) hk_get_created_java_vms, (void**) &orig_jni_get_created_java_vms) != MH_OK) {
 		OutputDebugStringA("disabler: Failed to create hook for JNI_GetCreatedJavaVMs");
@@ -57,6 +65,12 @@ bool initialize_hooks() {
 
 	if (MH_CreateHook(windows_os_dll_load, (LPVOID) hk_windows_os_dll_load, (void**) &orig_windows_os_dll_load) != MH_OK) {
 		OutputDebugStringA("disabler: Failed to create hook for Windows OS DLL load");
+		MH_Uninitialize();
+		return false;
+	}
+
+	if (MH_CreateHook(class_check, (LPVOID) hk_class_check, (void**) &orig_class_check) != MH_OK) {
+		OutputDebugStringA("disabler: Failed to create hook for class check");
 		MH_Uninitialize();
 		return false;
 	}
